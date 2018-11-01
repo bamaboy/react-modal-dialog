41732cfaa21e0cb5490262492a731c3b43b575fb
Sergey Tyupaev <tyupaevs@gmail.com>
2018-11-02 00:10:10 +0300

    chore(*): update react version

--- src/ModalContainer.js
+++ src/ModalContainer.js
@@ -1,4 +1,6 @@
-import React, { PropTypes } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
+
 
 import ModalPortal from './ModalPortal';
 import ModalBackground from './ModalBackground';
--- src/ModalPortal.js
+++ src/ModalPortal.js
@@ -1,4 +1,6 @@
-import React, { PropTypes } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
+
 import ReactDOM from 'react-dom';
 
 // Render into subtree is necessary for parent contexts to transfer over
